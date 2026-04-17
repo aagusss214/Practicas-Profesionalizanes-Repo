ca8e2dcf15c0f48c6aa7ddedb6a5925a25778de9
aagusss214 <elsupertv963@example.com>
2026-04-17 17:21:49 -0300

    commit new changes

--- editar.php
+++ editar.php
@@ -13,11 +13,18 @@ $fila = mysqli_fetch_assoc($resultado);
 
 <form action="actualizar.php" method="POST">
     <input type="hidden" name="id" value="<?php echo $fila['id']; ?>">
+    <label for="">Nombre y apellido</label>
     <input type="text" name="nombre_apellido" value="<?php echo $fila['nombre_apellido']; ?>">
+    <br><br>
     <input type="text" name="dni" value="<?php echo $fila['dni']; ?>">
+    <br><br>
     <input type="text" name="celular" value="<?php echo $fila['celular']; ?>">
+    <br><br>
     <input type="date" name="fecha_de_nacimiento" value="<?php echo $fila['fecha_de_nacimiento']; ?>">
+    <br><br>
     <input type="email" name="correo" value="<?php echo $fila['correo']; ?>">
+    <br><br>
     <input type="text" name="direccion" value="<?php echo $fila['direccion']; ?>">
+    <br><br>
     <input type="submit" value="Actualizar">
 </form>
